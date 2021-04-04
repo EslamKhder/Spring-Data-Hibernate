6c3dba769a88902341afbf592eb52e1dfbabd8e8
EslamKhder <eslamkhder81@gmail.com>
2021-04-05 01:33:35 +0200

    Create session & sessionFactory

--- src/com/springdata/hibernate/MainApp.java
+++ src/com/springdata/hibernate/MainApp.java
@@ -3,28 +3,39 @@ package com.springdata.hibernate;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import com.mysql.cj.jdbc.Driver;
+import com.springdata.hibernate.model.Client;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String url = "jdbc:mysql://localhost:3306/employee?useSSL=false";
-		String username = "eslam";
-		String password = "eslam";
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")			
+				.addAnnotatedClass(Client.class)		
+				.buildSessionFactory();
 		
-		try {
-			Connection connection = DriverManager.getConnection(
-					url, username ,password
-					);
-			System.out.println("Connected !");
-			System.out.println(connection);
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println(e.toString());
-		}
+		Session session = factory.getCurrentSession();
 		
-
+		
 	}
 
 }
+/*String url = "jdbc:mysql://localhost:3306/employee?useSSL=false";
+String username = "eslam";
+String password = "eslam";
+
+try {
+	Connection connection = DriverManager.getConnection(
+			url, username ,password
+			);
+	System.out.println("Connected !");
+	System.out.println(connection);
+} catch (Exception e) {
+	// TODO: handle exception
+	System.out.println(e.toString());
+}
+*/
