621606752d19c41a29597b6bf3f8eba55840bbdb
EslamKhder <eslamkhder81@gmail.com>
2021-04-05 01:21:17 +0200

    add Annotation in model Client

--- src/com/springdata/hibernate/model/Client.java
+++ src/com/springdata/hibernate/model/Client.java
@@ -1,13 +1,25 @@
 package com.springdata.hibernate.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client")
 public class Client {
 
+	@Id
+	@Column(name = "client_id")
 	private Long id;
 	
+	@Column(name = "client_name")
 	private String fullName;
 	
+	@Column(name = "client_age")
 	private int age;
 	
+	@Column(name = "client_address")
 	private String address;
 
 	public Long getId() {
