454f7904b8bdaa01b491b6ae3e46aab49dd19627
EslamKhder <eslamkhder81@gmail.com>
2021-04-05 01:08:51 +0200

    create model Client

---
+++ src/com/springdata/hibernate/model/Client.java
@@ -0,0 +1,46 @@
+package com.springdata.hibernate.model;
+
+public class Client {
+
+	private Long id;
+	
+	private String fullName;
+	
+	private int age;
+	
+	private String address;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
