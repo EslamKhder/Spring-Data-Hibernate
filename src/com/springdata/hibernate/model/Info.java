17fbe1f50e22dde22474dbd094b21045ae32a824
EslamKhder <eslamkhder81@gmail.com>
2021-04-10 10:37:28 +0200

    relationship  Models (Student - Info)

--- src/com/springdata/hibernate/model/Info.java
+++ src/com/springdata/hibernate/model/Info.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -18,10 +20,27 @@ public class Info {
 	
 	@Column(name = "phone")
 	private String phone;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_student")
+	private Student student;
+	
 
 	public Info() {
 		
 	}
+	
+	
+	public Student getStudent() {
+		return student;
+	}
+
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+
 	public int getId() {
 		return id;
 	}
--- src/com/springdata/hibernate/model/Student.java
+++ src/com/springdata/hibernate/model/Student.java
@@ -1,10 +1,14 @@
 package com.springdata.hibernate.model;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,10 +23,26 @@ public class Student {
 	@Column(name = "full_name")
 	private String name;
 	
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "student")
+	private Set<Info> infos;
+	
 	
 	public Student() {
 		
 	}
+
+	
+
+	public Set<Info> getInfos() {
+		return infos;
+	}
+
+
+
+	public void setInfos(Set<Info> infos) {
+		this.infos = infos;
+	}
+
 
 
 	public int getId() {
